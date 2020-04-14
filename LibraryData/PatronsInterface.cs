cff6901b7c55870f3cb738c83b5ce03c974f5f0f
KopiGod <kopibryant55@gmail.com>
2020-04-14 12:06:52 +0800

    Patrons Service

---
+++ LibraryData/PatronsInterface.cs
@@ -0,0 +1,22 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+	public interface PatronsInterface
+	{
+		//add Patrons
+		void addPatrons(Patron newPatron);
+
+		//GetAllProterties from Patron
+		Patron getPatron(int patronId);
+
+		//Get collection of Patron, Checkout, CheckoutHistory, Hold
+		IEnumerable<Patron> getAllPatron(int patronId);
+		IEnumerable<Checkouts> getAllCheckout(int patronId);
+		IEnumerable<CheckoutHistory> getAllCheckOutHistory(int patronId);
+		IEnumerable<Holds> getAllHold(int patronId);
+	}
+}
---
+++ LibraryServices/PatronsServices.cs
@@ -0,0 +1,52 @@
+﻿using LibraryData;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryServices
+{
+	public class PatronsServices : PatronsInterface
+	{
+		private readonly LibraryContext _context;
+
+		public PatronsServices(LibraryContext context) {
+			_context = context;
+		}
+		public void addPatrons(Patron newPatron)
+		{
+			_context.Add(newPatron);
+			_context.SaveChanges();
+		}
+		public Patron getPatron(int patronId)
+		{
+			return _context.Patrons
+				.Include(p => p.libraryCard)
+				.Include(p => p.LibraryBranch)
+				.FirstOrDefault(p => p.patronId == patronId);
+		}
+
+		public IEnumerable<Checkouts> getAllCheckout(int patronId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<CheckoutHistory> getAllCheckOutHistory(int patronId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Holds> getAllHold(int patronId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Patron> getAllPatron(int patronId)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
